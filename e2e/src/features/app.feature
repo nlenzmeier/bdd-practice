7a0036c4d5dfec308e2d5016ef340897eb9cfdd4
Nicolle Lenzmeier <niclenz@hotmail.com>
2020-02-08 11:00:24 -0600

    create links and add scenario to test links

--- e2e/src/features/app.feature
+++ e2e/src/features/app.feature
@@ -12,7 +12,19 @@ Feature: Go to Home Page
     Given I click the Display Text button
     Then I should see Hi there!
 
-  Scenario:
+  Scenario: I see a table
     Given I do nothing
     Then I should see a table with multiple rows
+
+  Scenario:
+    Given I do nothing
+    When I click <linkOpion>
+    Then I should be redirected to <site>
+
+    | linkOption | site                                  |
+    | Google     | https://www.google.com/?client=safari |
+    | Apple      | https://www.apple.com                 |
+    | Samsung    | https://www.samsung.com/us/1/         |
+
+
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { PageTwoComponent } from './page-two/page-two.component';
+import { PageThreeComponent } from './page-three/page-three.component';
 
 const appRoutes: Routes = [
   {path: 'home', component: AppComponent},
-  {path: 'page-2', component: PageTwoComponent}
+  {path: 'page-2', component: PageTwoComponent},
+  {path: 'page-3', component: PageThreeComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     PageTwoComponent,
+    PageThreeComponent,
   ],
   imports: [
     BrowserModule,
